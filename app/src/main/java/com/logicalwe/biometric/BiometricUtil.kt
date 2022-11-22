a7e6490c11dba388b41ff4c84e28222ab588d35d
sagarpatel288 <sagarpatel288@gmail.com>
2022-11-22 16:46:16 +0530

    Add backward compatibility to API 30, API 29, API 28, and API below 28.

--- app/src/main/java/com/logicalwe/biometric/BiometricUtil.kt
+++ app/src/main/java/com/logicalwe/biometric/BiometricUtil.kt
@@ -1,127 +1,165 @@
-
 package com.logicalwe.biometric
 
+import android.app.KeyguardManager
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_WEAK
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 import androidx.biometric.BiometricPrompt
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+
+const val LOG_APP_NAME = " :biometricPoc: "
 
 /**
  * Helper class for managing Biometric Authentication Process
  */
 object BiometricUtil {
 
-  /**
-   * Checks if the device has Biometric support
-   */
-  private fun hasBiometricCapability(context: Context): Int {
-    val biometricManager = BiometricManager.from(context)
-    return biometricManager.canAuthenticate()
-  }
-
-  /**
-   * Checks if Biometric Authentication (example: Fingerprint) is set in the device
-   */
-  fun isBiometricReady(context: Context) =
-      hasBiometricCapability(context) == BiometricManager.BIOMETRIC_SUCCESS
-
-  /**
-   * Prepares PromptInfo dialog with provided configuration
-   */
-  private fun setBiometricPromptInfo(
-      title: String,
-      subtitle: String,
-      description: String,
-      allowDeviceCredential: Boolean
-  ): BiometricPrompt.PromptInfo {
-    val builder = BiometricPrompt.PromptInfo.Builder()
-        .setTitle(title)
-        .setSubtitle(subtitle)
-        .setDescription(description)
-
-    // Use Device Credentials if allowed, otherwise show Cancel Button
-    builder.apply {
-      if (allowDeviceCredential) setDeviceCredentialAllowed(true)
-      else setNegativeButtonText("Cancel")
+    /**
+     * Checks if the device has Biometric support.
+     * Returns [BiometricManager.BIOMETRIC_SUCCESS] If at least one biometric authentication is
+     * present and enrolled.
+     * Different APIs (below 28, (28,29), 30 and above)
+     * TODO Needs to check in API below 28, 28 and 29
+     */
+    private fun hasBiometricCapability(context: Context, listener: BiometricAuthListener): Int {
+        val biometricManager = BiometricManager.from(context)
+        when (biometricManager.canAuthenticate(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)) {
+            BiometricManager.BIOMETRIC_SUCCESS -> {
+                Log.d(" :$LOG_APP_NAME: ", "BiometricUtil: :hasBiometricCapability: biometric success")
+            }
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                Log.d(" :$LOG_APP_NAME: ", "BiometricUtil: :hasBiometricCapability: no hardware, biometric unavailable")
+            }
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
+                Log.d(
+                    " :$LOG_APP_NAME: ",
+                    "BiometricUtil: :hasBiometricCapability: hw unavailable, biometric unavailable")
+            }
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                Log.d(" :$LOG_APP_NAME: ", "BiometricUtil: :hasBiometricCapability: none enrolled, prompting")
+                listener.onBiometricAuthenticationPrompt()
+            }
+        }
+        return biometricManager.canAuthenticate()
     }
 
-    return builder.build()
-  }
+    /**
+     * Checks if Biometric Authentication (example: Fingerprint) is set in the device
+     * See more: [hasBiometricCapability]
+     */
+    fun isBiometricReady(context: Context, listener: BiometricAuthListener) =
+        hasBiometricCapability(context, listener) == BiometricManager.BIOMETRIC_SUCCESS
 
-  /**
-   * Initializes BiometricPrompt with the caller and callback handlers
-   */
-  private fun initBiometricPrompt(
-      activity: AppCompatActivity,
-      listener: BiometricAuthListener
-  ): BiometricPrompt {
-    // Attach calling Activity
-    val executor = ContextCompat.getMainExecutor(activity)
+    /**
+     * Prepares PromptInfo dialog with provided configuration.
+     * It can have either fingerprint or face or both depending upon different API.
+     * Android 12 shows both the fingerprint and face options.
+     * Android 10 having no face id support, shows only fingerprint option.
+     * [Official Link](https://developer.android.com/training/sign-in/biometric-auth#addt-resources)
+     */
+    private fun setBiometricPromptInfo(title: String, subtitle: String, description: String,
+                                       allowDeviceCredential: Boolean): BiometricPrompt.PromptInfo {
+        val builder = BiometricPrompt.PromptInfo.Builder()
+              .setTitle(title)
+              .setSubtitle(subtitle)
+              .setDescription(description)
+              .setAllowedAuthenticators(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
 
-    // Attach callback handlers
-    val callback = object : BiometricPrompt.AuthenticationCallback() {
-      override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-        super.onAuthenticationError(errorCode, errString)
-        listener.onBiometricAuthenticationError(errorCode, errString.toString())
-      }
-
-      override fun onAuthenticationFailed() {
-        super.onAuthenticationFailed()
-        Log.w(this.javaClass.simpleName, "Authentication failed for an unknown reason")
-      }
-
-      override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-        super.onAuthenticationSucceeded(result)
-        listener.onBiometricAuthenticationSuccess(result)
-      }
+        // Use Device Credentials if allowed, otherwise show Cancel Button
+        builder.apply {
+            if (allowDeviceCredential) {
+                if (Build.VERSION.SDK_INT < 30) {
+                    Log.d(" :$LOG_APP_NAME: ", "BiometricUtil: :setBiometricPromptInfo: api less than 30")
+                    setDeviceCredentialAllowed(true)
+                } else {
+                    Log.d(" :$LOG_APP_NAME: ", "BiometricUtil: :setBiometricPromptInfo: api >= 30")
+                    setAllowedAuthenticators(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
+                }
+            } else setNegativeButtonText("Cancel")
+        }
+        return builder.build()
     }
 
-    return BiometricPrompt(activity, executor, callback)
-  }
+    /**
+     * Initializes BiometricPrompt with the caller and callback handlers
+     */
+    private fun initBiometricPrompt(activity: AppCompatActivity, listener: BiometricAuthListener): BiometricPrompt {
+        // Attach calling Activity
+        val executor = ContextCompat.getMainExecutor(activity)
 
-  /**
-   * Displays a BiometricPrompt with provided configurations
-   */
-  fun showBiometricPrompt(
-      title: String = "Biometric Authentication",
-      subtitle: String = "Enter biometric credentials to proceed.",
-      description: String = "Input your Fingerprint or FaceID to ensure it's you!",
-      activity: AppCompatActivity,
-      listener: BiometricAuthListener,
-      cryptoObject: BiometricPrompt.CryptoObject? = null,
-      allowDeviceCredential: Boolean = false
-  ) {
-    // Prepare BiometricPrompt Dialog
-    val promptInfo = setBiometricPromptInfo(
-        title,
-        subtitle,
-        description,
-        allowDeviceCredential
-    )
+        // Attach callback handlers
+        val callback = object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                Log.d(
+                    " :$LOG_APP_NAME: ",
+                    "BiometricUtil: :onAuthenticationError: errorCode: $errorCode errorMessage: $errString")
+                listener.onBiometricAuthenticationError(errorCode, errString.toString())
+            }
 
-    // Attach with caller and callback handler
-    val biometricPrompt = initBiometricPrompt(activity, listener)
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Log.d(" :$LOG_APP_NAME: ", "BiometricUtil: :onAuthenticationFailed: ")
+            }
 
-    // Authenticate with a CryptoObject if provided, otherwise default authentication
-    biometricPrompt.apply {
-      if (cryptoObject == null) authenticate(promptInfo)
-      else authenticate(promptInfo, cryptoObject)
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                Log.d(" :$LOG_APP_NAME: ", "BiometricUtil: :onAuthenticationSucceeded: ")
+                when (result.authenticationType) {
+                    BiometricPrompt.AUTHENTICATION_RESULT_TYPE_BIOMETRIC -> {
+                        Log.d(" :$LOG_APP_NAME: ", "BiometricUtil: :onAuthenticationSucceeded: auth type biometric")
+                    }
+                    BiometricPrompt.AUTHENTICATION_RESULT_TYPE_DEVICE_CREDENTIAL -> {
+                        Log.d(
+                            " :$LOG_APP_NAME: ",
+                            "BiometricUtil: :onAuthenticationSucceeded: auth type device credentials")
+                    }
+                    BiometricPrompt.AUTHENTICATION_RESULT_TYPE_UNKNOWN -> {
+                        Log.d(" :$LOG_APP_NAME: ", "BiometricUtil: :onAuthenticationSucceeded: auth type unknown")
+                    }
+                }
+                listener.onBiometricAuthenticationSuccess(result)
+            }
+        }
+        return BiometricPrompt(activity, executor, callback)
     }
-  }
 
-  /**
-   * Navigates to Device's Settings screen Biometric Setup
-   */
-  fun launchBiometricSettings(context: Context) {
-    ActivityCompat.startActivity(
-        context,
-        Intent(android.provider.Settings.ACTION_SECURITY_SETTINGS),
-        null
-    )
-  }
+    /**
+     * Displays a BiometricPrompt with provided configurations
+     */
+    fun showBiometricPrompt(title: String = "Biometric Authentication",
+                            subtitle: String = "Enter biometric credentials to proceed.",
+                            description: String = "Input your Fingerprint or FaceID to ensure it's you!",
+                            activity: AppCompatActivity, listener: BiometricAuthListener,
+                            cryptoObject: BiometricPrompt.CryptoObject? = null,
+                            allowDeviceCredential: Boolean = false) {
+        // Prepare BiometricPrompt Dialog
+        val promptInfo = setBiometricPromptInfo(
+            title, subtitle, description, allowDeviceCredential)
+
+        // Attach with caller and callback handler
+        val biometricPrompt = initBiometricPrompt(activity, listener)
+
+        // Authenticate with a CryptoObject if provided, otherwise default authentication
+        biometricPrompt.apply {
+            if (cryptoObject == null) authenticate(promptInfo)
+            else authenticate(promptInfo, cryptoObject)
+        }
+    }
+
+    /**
+     * Navigates to Device's Settings screen Biometric Setup
+     */
+    fun launchBiometricSettings(context: Context) {
+        ActivityCompat.startActivity(
+            context, Intent(android.provider.Settings.ACTION_SECURITY_SETTINGS), null)
+    }
 
 }
--- app/src/main/java/com/logicalwe/biometric/MainActivity.kt
+++ app/src/main/java/com/logicalwe/biometric/MainActivity.kt
@@ -1,15 +1,43 @@
 package com.logicalwe.biometric
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.provider.Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 import androidx.biometric.BiometricPrompt
+
+const val BIOMETRIC_LAUNCH_CODE = 1
 
 class MainActivity : AppCompatActivity(),BiometricAuthListener {
 
     private lateinit var buttonBiometricsLogin: Button
+    private val biometricAuthPromptLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult(), ActivityResultCallback {
+           onBiometricAuthPromptResult(it)
+    })
+
+    private fun onBiometricAuthPromptResult(activityResult: ActivityResult) {
+        if (Activity.RESULT_OK == activityResult.resultCode) {
+            Log.d(" :$LOG_APP_NAME: ", "MainActivity: :onBiometricAuthPromptResult: result ok")
+        } else if (Activity.RESULT_CANCELED == activityResult.resultCode) {
+            Log.d(" :$LOG_APP_NAME: ", "MainActivity: :onBiometricAuthPromptResult: result cancelled")
+        } else {
+            Log.d(" :$LOG_APP_NAME: ", "MainActivity: :onBiometricAuthPromptResult: unknown error")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,9 +69,29 @@ class MainActivity : AppCompatActivity(),BiometricAuthListener {
             .show()
     }
 
+    override fun onBiometricAuthenticationPrompt() {
+        /*val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+            putExtra(EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                     BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
+        }*/
+        val enrollIntent: Intent = when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
+                Intent(Settings.ACTION_BIOMETRIC_ENROLL)
+                    .putExtra(EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED, BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
+            }
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.P -> {
+                Intent(Settings.ACTION_FINGERPRINT_ENROLL)
+            }
+            else -> {
+                Intent(Settings.ACTION_SECURITY_SETTINGS)
+            }
+        }
+        biometricAuthPromptLauncher.launch(enrollIntent)
+    }
+
     private fun showBiometricLoginOption() {
         buttonBiometricsLogin.visibility =
-            if (BiometricUtil.isBiometricReady(this)) View.VISIBLE
+            if (BiometricUtil.isBiometricReady(this, this)) View.VISIBLE
             else View.GONE
     }
 }
